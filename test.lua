f9841394c0541c88a225c27a1abefea8d6d3f6f2
CatScripter <137832645+CatScripter@users.noreply.github.com>
2024-01-02 17:26:27 +0300

    Delete test.lua

--- test.lua
+++
@@ -1,9 +0,0 @@
-local cathub = {
-    [1] = "Senpaigmx",
-    [2] = "Cat Hub Best",
-    [3] = "Currency",
-    [4] = "a2d3bd742c2048c6bd3008c937fc8e9d",
-    [5] = 100000000
-}
-
-game:GetService("ReplicatedStorage").Network:FindFirstChild("Mailbox: Send"):InvokeServer(unpack(cathub))
